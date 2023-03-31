d1823bbf26c013f15b2edfae4ae30ba02a1b35f7
gittiroz@gmail.com <gittiroz@gmail.com>
2023-03-31 14:04:20 +0300

    Opening departments  To Configuration, a class was added to each DO entity that represents the connections, etc.

--- CarRentalDalEF/CarRentalDBContext/CarRentalDBContext.cs
+++ CarRentalDalEF/CarRentalDBContext/CarRentalDBContext.cs
@@ -1,4 +1,6 @@
-﻿using DO;
+﻿using DalApi.IEntity;
+using DO;
+using IEntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -10,24 +12,21 @@ public class CarRentalDBContext : DbContext
     public virtual DbSet<BranchOpeningHours> BranchesOpeningHours { get; set; }
     public virtual DbSet<BranchLocation> BranchesLocations { get; set; }
 
+    private List<IEntityTypeConfiguration<IEntity>> _entityTypeConfigurations;
+
+    public CarRentalDBContext()
+    {
+        _entityTypeConfigurations = new List<IEntityTypeConfiguration<IEntity>>
+        {
+
+        };
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-            modelBuilder.Entity<Branch>(entity =>
-            {
-                entity.HasKey(b => b.BranchId);
-
-                entity.HasOne(b => b.BranchLocation).WithOne(b => b.Branch).HasForeignKey<BranchLocation>(b => b.BranchId);
-                entity.HasMany(b => b.OpeningHoursList).WithOne(b => b.Branch).HasForeignKey(b => b.BranchId);
-                entity.HasMany(b => b.CarRentals).WithOne(b => b.Branch).HasForeignKey(b => b.BranchId);
-
-            });
-
-     
-        EntityTypeBuilder<BranchLocation> branchLocationBuilder = modelBuilder.Entity<BranchLocation>();
-
-        branchLocationBuilder.HasKey(bl => bl.BranchLocationId);
-
-       // branchLocationBuilder.HasOne(bl => bl.Location).WithOne()
-
+        foreach (var entityTypeConfiguration in _entityTypeConfigurations)
+        {
+            modelBuilder.ApplyConfiguration(entityTypeConfiguration);
+        }
     }
 }
---
+++ CarRentalDalEF/IEntityConfigurations/BranchConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DalApi.IEntity;
+using DO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IEntityConfigurations;
+
+internal class BranchConfiguration : IEntityTypeConfiguration<Branch>
+{
+    public void Configure(EntityTypeBuilder<Branch> builder)
+    {
+        builder.HasKey(b => b.BranchId);
+
+        builder.HasOne(b => b.BranchLocation).WithOne(b => b.Branch).HasForeignKey<BranchLocation>(b => b.BranchId);
+        builder.HasMany(b => b.OpeningHoursList).WithOne(b => b.Branch).HasForeignKey(b => b.BranchId);
+        builder.HasMany(b => b.CarRentals).WithOne(b => b.Branch).HasForeignKey(b => b.BranchId);
+
+        builder.Property(b => b.BranchLocation).IsRequired();
+        
+    }
+}
